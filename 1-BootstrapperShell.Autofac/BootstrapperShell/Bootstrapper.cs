e1d71cf15f0694c947fed7cc6a560c841b1da6e8
Jeremy Ellis <jeremy@ellisnet.com>
2017-04-05 13:10:28 -0500

    Work in progress on Prism.Autofac.Wpf samples - looking at possible updates to Autofac container usage. Abandoning project for now after discussion with Brian L.

---
+++ 1-BootstrapperShell.Autofac/BootstrapperShell/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Prism.Autofac;
+using BootstrapperShell.Views;
+using System.Windows;
+
+namespace BootstrapperShell
+{
+    class Bootstrapper : AutofacBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
